564455f0c20a9c8fe79db1353b02bc1626fde596
bezo97 <bezozo97@gmail.com>
2020-11-15 16:28:20 +0100

    DXInterop without SharpDX - bugfix: dx context was created twice - document acronyms, confusing parts

---
+++ src/GLWpfControl/Interop/DXInterop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace OpenTK.Wpf.Interop
+{
+    internal static class DXInterop
+    {
+        public const uint DefaultSdkVersion = 32;
+        private const int CreateDeviceEx_Offset = 20;
+        private const int CreateRenderTarget_Offset = 28;
+
+        private delegate int NativeCreateDeviceEx(IntPtr contextHandle, int adapter, DeviceType deviceType, IntPtr focusWindowHandle, CreateFlags behaviorFlags, ref PresentationParameters presentationParameters, IntPtr fullscreenDisplayMode, out IntPtr deviceHandle);
+        private delegate int NativeCreateRenderTarget(IntPtr deviceHandle, int width, int height, Format format, MultisampleType multisample, int multisampleQuality, bool lockable, out IntPtr surfaceHandle, ref IntPtr sharedHandle);
+
+        [DllImport("d3d9.dll")]
+        public static extern int Direct3DCreate9Ex(uint SdkVersion, out IntPtr ctx);
+
+        public static int CreateDeviceEx(IntPtr contextHandle, int adapter, DeviceType deviceType, IntPtr focusWindowHandle, CreateFlags behaviorFlags, ref PresentationParameters presentationParameters, IntPtr fullscreenDisplayMode, out IntPtr deviceHandle)
+        {
+            IntPtr vTable = Marshal.ReadIntPtr(contextHandle, 0);
+            IntPtr functionPointer = Marshal.ReadIntPtr(vTable, CreateDeviceEx_Offset * IntPtr.Size);
+            NativeCreateDeviceEx method = Marshal.GetDelegateForFunctionPointer<NativeCreateDeviceEx>(functionPointer);
+            return method(contextHandle, adapter, deviceType, focusWindowHandle, behaviorFlags, ref presentationParameters, fullscreenDisplayMode, out deviceHandle);
+        }
+
+        public static int CreateRenderTarget(IntPtr deviceHandle, int width, int height, Format format, MultisampleType multisample, int multisampleQuality, bool lockable, out IntPtr surfaceHandle, ref IntPtr sharedHandle)
+        {
+            IntPtr vTable = Marshal.ReadIntPtr(deviceHandle, 0);
+            IntPtr functionPointer = Marshal.ReadIntPtr(vTable, CreateRenderTarget_Offset * IntPtr.Size);
+            NativeCreateRenderTarget method = Marshal.GetDelegateForFunctionPointer<NativeCreateRenderTarget>(functionPointer);
+            return method(deviceHandle, width, height, format, multisample, multisampleQuality, lockable, out surfaceHandle, ref sharedHandle);
+        }
+
+    }
+
+}
---
+++ src/GLWpfControl/Interop/Enums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace OpenTK.Wpf.Interop
+{
+    [Flags]
+    internal enum CreateFlags
+    {
+        Multithreaded = 4,
+        PureDevice = 16,
+        HardwareVertexProcessing = 64,
+
+    }
+
+    internal enum DeviceType
+    {
+        /// <summary>
+        /// Hardware rasterization. Shading is done with software, hardware, or mixed transform and lighting.
+        /// </summary>
+        HAL = 1,
+
+    }
+
+    internal enum Format
+    {
+        Unknown = 0,
+        /// <summary>
+        /// 32-bit ARGB pixel format with alpha, using 8 bits per channel.
+        /// </summary>
+        A8R8G8B8 = 21,
+        /// <summary>
+        /// 32-bit RGB pixel format, where 8 bits are reserved for each color.
+        /// </summary>
+        X8R8G8B8 = 22, 
+
+    }
+
+    internal enum MultisampleType
+    {
+        None = 0,
+
+    }
+
+    internal enum SwapEffect
+    {
+        Discard = 1,
+
+    }
+
+}
