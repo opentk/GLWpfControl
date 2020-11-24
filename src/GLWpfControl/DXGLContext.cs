445b4176d772f483cea0a08a1687c18f2adcd99f
Varon <varon@users.noreply.github.com>
2020-11-24 19:34:36 +0200

    Better settings/shared context creation

--- src/GLWpfControl/DXGLContext.cs
+++ src/GLWpfControl/DXGLContext.cs
@@ -30,13 +30,14 @@ namespace OpenTK.Wpf {
 
         /// The shared context we (may) want to lazily create/use.
         private static IGraphicsContext _sharedContext;
+        private static GLWpfControlSettings _sharedContextSettings;
         /// List of extra resources to dispose along with the shared context.
         private static IDisposable[] _sharedContextResources;
         /// The number of active controls using the shared context.
         private static int _sharedContextReferenceCount;
 
 
-        public DxGlContext([CanBeNull] IGraphicsContext graphicsContext) {
+        public DxGlContext([NotNull] GLWpfControlSettings settings) {
             DXInterop.Direct3DCreate9Ex(DXInterop.DefaultSdkVersion, out var dxContextHandle);
             DxContextHandle = dxContextHandle;
 
@@ -72,27 +73,39 @@ namespace OpenTK.Wpf {
             DxDeviceHandle = dxDeviceHandle;
 
             // if the graphics context is null, we use the shared context.
-            if (graphicsContext == null) {
-                graphicsContext = RetrieveSharedOpenGLContext();
+            if (settings.ContextToUse != null) {
+                GraphicsContext = settings.ContextToUse;
+            }
+            else {
+                GraphicsContext = GetOrCreateSharedOpenGLContext(settings);
             }
 
-            GraphicsContext = graphicsContext;
             GlDeviceHandle = Wgl.DXOpenDeviceNV(dxDeviceHandle);
         }
 
-        private static IGraphicsContext RetrieveSharedOpenGLContext() {
-            if (_sharedContext == null) {
+        private static IGraphicsContext GetOrCreateSharedOpenGLContext(GLWpfControlSettings settings) {
+            if (_sharedContext != null) {
+                var isSameContext = GLWpfControlSettings.WouldResultInSameContext(settings, _sharedContextSettings);
+                if (!isSameContext) {
+                    throw new ArgumentException($"The provided {nameof(GLWpfControlSettings)} would result" +
+                                                $"in a different context creation to one previously created. To fix this," +
+                                                $" either ensure all of your context settings are identical, or provide an " +
+                                                $"external context via the '{nameof(GLWpfControlSettings.ContextToUse)}' field.");
+                }
+            } 
+            
+            else {
                 var nws = NativeWindowSettings.Default;
                 nws.StartFocused = false;
                 nws.StartVisible = false;
                 nws.NumberOfSamples = 0;
                 // if we ask GLFW for 1.0, we should get the highest level context available with full compat.
-                nws.APIVersion = new Version(1, 0);
-                nws.Flags = ContextFlags.Offscreen;
+                nws.APIVersion = new Version(settings.MajorVersion, settings.MinorVersion);
+                nws.Flags = ContextFlags.Offscreen | settings.GraphicsContextFlags;
                 // we have to ask for any compat in this case.
-                nws.Profile = ContextProfile.Any;
-                nws.WindowBorder = WindowBorder.Hidden;
-                nws.WindowState = WindowState.Minimized;
+                nws.Profile = settings.GraphicsProfile;
+                // nws.WindowBorder = WindowBorder.Hidden;
+                // nws.WindowState = WindowState.Minimized;
                 var glfwWindow = new NativeWindow(nws) {IsVisible = false};
                 var provider = new GLFWBindingsContext();
                 Wgl.LoadBindings(provider);
@@ -117,9 +130,12 @@ namespace OpenTK.Wpf {
         }
 
         public void Dispose() {
-            if (Interlocked.Decrement(ref _sharedContextReferenceCount) == 0) {
-                foreach (var resource in _sharedContextResources) {
-                    resource.Dispose();
+            // we only dispose of the graphics context if we're using the shared one.
+            if (ReferenceEquals(_sharedContext, GraphicsContext)) {
+                if (Interlocked.Decrement(ref _sharedContextReferenceCount) == 0) {
+                    foreach (var resource in _sharedContextResources) {
+                        resource.Dispose();
+                    }
                 }
             }
         }
