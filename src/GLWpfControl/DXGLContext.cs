bbcdf09ff0817366759f4aeca0ef66b26f5b5a60
Julius Häger <julius_hager@hotmail.com>
2025-07-03 01:46:11 +0200

    Remove old comment.

--- src/GLWpfControl/DXGLContext.cs
+++ src/GLWpfControl/DXGLContext.cs
@@ -111,7 +111,6 @@ namespace OpenTK.Wpf
                 AutoDepthStencilFormat = Format.Unknown,
                 BackBufferCount = 1,
                 EnableAutoDepthStencil = 0,
-                // Add D3DPRESENTFLAG_DISCARD_DEPTHSTENCIL?
                 Flags = 0,
                 FullScreen_RefreshRateInHz = 0,
                 MultiSampleQuality = 0,
